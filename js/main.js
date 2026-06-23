d3063bf365c1101a385498af288292f5ed387dcc
Jillian Nerice Castillo <jbcastil@andrew.cmu.edu>
2026-06-23 12:58:06 +0300

    clean version without dataset

---
+++ js/main.js
@@ -0,0 +1,18 @@
+async function loadArtists() {
+  const response = await fetch("data/Artists.json");
+  const artists = await response.json();
+
+  console.log(artists); // check in console
+
+  const container = document.getElementById("output");
+
+  container.innerHTML = "";
+
+  artists.slice(0, 10).forEach(artist => {
+    const div = document.createElement("div");
+
+    div.innerText = artist.DisplayName + " (" + artist.Nationality + ")";
+
+    container.appendChild(div);
+  });
+}
